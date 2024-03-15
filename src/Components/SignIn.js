98999465c00e6899b854a326bc5b0dae55845745
ravi Reddy <ravikanthreddy.n@gmail.com>
2024-03-15 09:44:22 -0700

    initial commit

---
+++ src/Components/SignIn.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+// components/SignIn.js
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <h2>Sign In Page</h2>
+      <p>Please sign in to access your account.</p>
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ src/Components/SignUp.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+// components/SignUp.js
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h2>Sign Up Page</h2>
+      <p>Please sign up to create an account.</p>
+    </div>
+  );
+}
+
+export default SignUp;
